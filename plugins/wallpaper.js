ec8f428b76c3f7875ac6308f3e0eac576575f038
AiDarkEzio <scienceposhitha@gmail.com>
2022-07-19 08:52:51 +0530

    Ai_Dark_Ezio is (add wallpaper for pattern) ✔

--- plugins/wallpaper.js
+++ plugins/wallpaper.js
@@ -17,7 +17,7 @@ const lang = ezio.getString("wallpaper");
 
 ezio.addCommand(
   {
-    pattern: [],
+    pattern: ['wallpaper'],
     desc: lang.WP,
     sucReact: "🌇",
     category: ["all", "downloade"],
